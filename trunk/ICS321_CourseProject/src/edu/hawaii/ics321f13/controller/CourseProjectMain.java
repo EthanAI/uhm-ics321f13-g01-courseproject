b39eef9ba1e2bfadf8e82558feb276436d95b0f5
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-06 01:47:51 +0000

    Eclipse project initial commit.

---
+++ trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/controller/CourseProjectMain.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package edu.hawaii.ics321f13.controller;
+
+/**
+ * This is the project Main-Class. Minimally functions to establish program "ready" state, after which control is
+ * passed to appropriate <code>Controller</code> instance.
+ * 
+ * @see Controller
+ * 
+ * @author Kyle Twogood
+ *
+ */
+public class CourseProjectMain {
+
+	/**
+	 * Program entry point. Serves as initializer and subsequently passes control to appropriate <code>Controller</code>
+	 * instance.
+	 * 
+	 * @param args - command-line supplied argument list
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Note to other group members: There is no "Controller" interface at the moment, we will need to define a
+		 * spec for that when we determine program control flow.
+		 * 
+		 * Also, in case you are not aware of the eclipse comment flags, here they are:
+		 * TODO Marks something that should be done before project is turned. Don't use this for feature requests. That will get really messy really fast.
+		 * FIXME Marks code which, in its current form, does not function as designed. This tag should be accompanied by a corresponding issue log in the Issue Tracker.
+		 * XXX This marks a workaround (a.k.a. hack) to get some section of code to work. This means that the solution is inefficient, inelegant, or otherwise needs revision. I totally understand
+		 * that you sometimes need to do this for whatever reason, just make sure you ALWAYS put this tag on the section of code (with a description) BEFORE you start hacking away. 
+		 * 
+		 * - Kyle
+		 */
+	}
+
+}
