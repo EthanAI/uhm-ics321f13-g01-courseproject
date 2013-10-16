32b9bcf5c16dd9f5df11d630037d394a3e74ee58
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-16 03:20:29 +0000

    Created default data model implementation class skeleton.

---
+++ ICS321_CourseProject/src/edu/hawaii/ics321f13/model/impl/DefaultDataModelFactory.java
@@ -0,0 +1,15 @@
+package edu.hawaii.ics321f13.model.impl;
+
+import edu.hawaii.ics321f13.model.interfaces.DataModelFactory;
+import edu.hawaii.ics321f13.model.interfaces.LoginInfo;
+import edu.hawaii.ics321f13.model.interfaces.SearchableModel;
+
+public class DefaultDataModelFactory implements DataModelFactory {
+
+	@Override
+	public SearchableModel fromLogin(LoginInfo login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
