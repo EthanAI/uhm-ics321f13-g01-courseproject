3ff372bea1caaad23ee9c9ea162cfb78de34b1f6
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-17 12:54:21 +0000

    Context-sensitive hiding of the search box text.

--- ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/DefaultView.java
+++ ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/DefaultView.java
@@ -1,12 +1,11 @@
 package edu.hawaii.ics321f13.view.impl;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
@@ -29,27 +28,18 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import javax.swing.ScrollPaneConstants;
 
-public class DefaultView extends JFrame {
+import edu.hawaii.ics321f13.model.interfaces.ImageResult;
+import edu.hawaii.ics321f13.model.interfaces.Traversable;
+import edu.hawaii.ics321f13.view.interfaces.ImageTransformer;
+import edu.hawaii.ics321f13.view.interfaces.View;
 
+public class DefaultView extends JFrame implements View {
+
+	/** Serialization support. */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
 	private JTextField txtSearch;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DefaultView frame = new DefaultView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
@@ -71,62 +61,64 @@ public class DefaultView extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBackground(Color.WHITE);
 		scrollPane.setForeground(Color.WHITE);
-		scrollPane.getViewport().setBorder(null);
 		scrollPane.getViewport().setBackground(Color.WHITE);
 		scrollPane.getViewport().setForeground(Color.WHITE);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 		scrollPane.setColumnHeaderView(null);
 		scrollPane.setViewportBorder(null);
+		scrollPane.setBorder(null);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 749, Short.MAX_VALUE)
+				.addComponent(panel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(10)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 729, Short.MAX_VALUE)
-					.addGap(10))
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 739, Short.MAX_VALUE)
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 520, Short.MAX_VALUE)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		
 		txtSearch = new JTextField();
+		txtSearch.setSelectionColor(Color.LIGHT_GRAY);
+		txtSearch.setSelectedTextColor(Color.WHITE);
+		txtSearch.setForeground(Color.LIGHT_GRAY);
 		txtSearch.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent e) {
-				txtSearch.setBorder(new LineBorder(Color.DARK_GRAY));
+				searchFieldStateChanged(true);
+				txtSearch.selectAll();
 			}
 
 			@Override
 			public void focusLost(FocusEvent e) {
-				txtSearch.setBorder(new LineBorder(Color.LIGHT_GRAY));
+				searchFieldStateChanged(false);
 			}
 		});
 		txtSearch.addMouseListener(new MouseAdapter() {
+			
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				System.out.println("Enter");
-				txtSearch.setBorder(new LineBorder(Color.DARK_GRAY));
+				searchFieldStateChanged(true);
 			}
 			
 			@Override
 			public void mouseExited(MouseEvent e) {
-				System.out.println("Exit");
-				txtSearch.setBorder(new LineBorder(Color.LIGHT_GRAY));
+				searchFieldStateChanged(false);
 			}
 		});
 		
-		txtSearch.setToolTipText("");
-		txtSearch.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		txtSearch.setForeground(Color.LIGHT_GRAY);
+		txtSearch.setBorder(BorderFactory.createCompoundBorder(new LineBorder(Color.LIGHT_GRAY), 
+				BorderFactory.createEmptyBorder(0, 10, 0, 10)));
 		txtSearch.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-		txtSearch.setText("  Search images...");
+		txtSearch.setText("Search images...");
 		txtSearch.setColumns(10);
 		
 		JButton btnNewButton = new JButton("\u2713");
@@ -207,5 +199,76 @@ public class DefaultView extends JFrame {
 		));
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
+	}
+
+	@Override
+	public void addActionListener(ActionListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setImageSource(Traversable<ImageResult> source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBusy(boolean busy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addImageTransformer(ImageTransformer transformer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addImageTransformer(ImageTransformer transformer, int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int removeImageTransformer(ImageTransformer transformer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ImageTransformer removeImageTransformer(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearImageTransformers() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private String lastSearchFieldText = "";
+	private boolean currentState = false;
+	private void searchFieldStateChanged(boolean active) {
+		if(currentState != active && active) {
+			currentState = active;
+			txtSearch.setBorder(BorderFactory.createCompoundBorder(new LineBorder(Color.GRAY), 
+					BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+			txtSearch.setText(lastSearchFieldText);
+		} else if(currentState != active && !active && txtSearch.getMousePosition() == null && !txtSearch.hasFocus()) {
+			currentState = active;
+			txtSearch.setBorder(BorderFactory.createCompoundBorder(new LineBorder(Color.LIGHT_GRAY), 
+					BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+			lastSearchFieldText = txtSearch.getText();
+			txtSearch.setText("  Search images...");
+		}
 	}
 }
