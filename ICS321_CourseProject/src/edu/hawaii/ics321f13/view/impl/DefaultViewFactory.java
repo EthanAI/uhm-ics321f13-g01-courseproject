1abe4bda81eea29fd81ef6cef64258dffd0a0473
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-17 12:55:00 +0000

    Implemented createView() method.

--- ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/DefaultViewFactory.java
+++ ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/DefaultViewFactory.java
@@ -1,5 +1,8 @@
 package edu.hawaii.ics321f13.view.impl;
 
+import java.io.IOException;
+
+import edu.hawaii.ics321f13.model.interfaces.LoginInfo;
 import edu.hawaii.ics321f13.model.interfaces.LoginPrompt;
 import edu.hawaii.ics321f13.view.interfaces.View;
 import edu.hawaii.ics321f13.view.interfaces.ViewFactory;
@@ -8,14 +11,40 @@ public class DefaultViewFactory implements ViewFactory {
 
 	@Override
 	public View createView() {
-		// TODO Auto-generated method stub
-		return null;
+		DefaultView view = new DefaultView();
+		view.setVisible(true);
+		return view;
 	}
 
 	@Override
 	public LoginPrompt createLoginPrompt() {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Debug: placeholder dummy code.
+		return new LoginPrompt() {
+
+			@Override
+			public LoginInfo getLoginInfo() {
+				return new LoginInfo() {
+
+					@Override
+					public void close() throws IOException {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public String getUserName() {
+						return "";
+					}
+
+					@Override
+					public char[] getPassword() {
+						return new char[0];
+					}
+					
+				};
+			};
+			
+		};
 	}
 
 }
