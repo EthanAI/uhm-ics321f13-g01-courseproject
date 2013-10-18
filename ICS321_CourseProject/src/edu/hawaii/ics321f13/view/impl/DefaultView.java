f230d5155174e8c64ed8a7dee8959eedfb66151e
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-18 08:00:54 +0000

    Update view. Removed buttons and replaced by custom navigation icons. It looks better.

--- ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/DefaultView.java
+++ ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/DefaultView.java
@@ -4,7 +4,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.RenderingHints;
 
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
@@ -46,7 +50,7 @@ public class DefaultView extends JFrame implements View {
 	 * Create the frame.
 	 */
 	public DefaultView() {
-		setTitle("Wiki Image Search");
+		setTitle("Wikipedia Image Search");
 		setFont(new Font("Segoe UI", Font.PLAIN, 12));
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -123,24 +127,23 @@ public class DefaultView extends JFrame implements View {
 		txtSearch.setText("Search images...");
 		txtSearch.setColumns(10);
 		
-		JButton btnNewButton = new JButton("\u2794");
-		btnNewButton.setFont(new Font("Segoe UI Symbol", btnNewButton.getFont().getStyle(), 30));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		JLabel btnNewButton = new JLabel("\u2794");
+		btnNewButton.setForeground(new Color(211, 211, 211));
+		btnNewButton.setFont(new Font("Segoe UI Symbol", btnNewButton.getFont().getStyle(), 70));
 		
-		JButton btnNewButton_1 = new JButton("\u2770");
-		btnNewButton_1.setFont(new Font("Segoe UI Symbol", btnNewButton_1.getFont().getStyle(), 30));
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		JLabel btnNewButton_1 = new JLabel("\u2770");
+		btnNewButton_1.setForeground(new Color(230, 230, 250));
+		btnNewButton_1.setFont(new Font("Segoe UI Symbol", btnNewButton_1.getFont().getStyle(), 60));
+		btnNewButton_1.setHorizontalAlignment(SwingConstants.CENTER);
+		btnNewButton_1.setBackground(Color.WHITE);
 		
-		JButton btnue = new JButton("\u2771");
-		btnue.setFont(new Font("Segoe UI Symbol", btnue.getFont().getStyle(), 30));
+		JLabel btnue = new JLabel("\u2771");
+		btnue.setForeground(new Color(230, 230, 250));
+		btnue.setHorizontalAlignment(SwingConstants.CENTER);
+		btnue.setFont(new Font("Segoe UI Symbol", btnue.getFont().getStyle(), 60));
 		
 		JLabel lblWikiImages = new JLabel("Wiki Images");
+		lblWikiImages.setFocusable(true);
 		lblWikiImages.setForeground(SystemColor.activeCaption);
 		lblWikiImages.setFont(new Font("Segoe UI Light", Font.PLAIN, 50));
 		lblWikiImages.setHorizontalAlignment(SwingConstants.CENTER);
@@ -155,9 +158,9 @@ public class DefaultView extends JFrame implements View {
 							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
 							.addComponent(txtSearch, GroupLayout.DEFAULT_SIZE, 496, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE)
 							.addGap(10)
-							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addComponent(btnue, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
@@ -166,17 +169,19 @@ public class DefaultView extends JFrame implements View {
 				.addGroup(gl_panel.createSequentialGroup()
 					.addGap(20)
 					.addComponent(lblWikiImages, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
-						.addComponent(btnue, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(txtSearch, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
-						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE))
-					.addContainerGap())
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnue, GroupLayout.PREFERRED_SIZE, 47, Short.MAX_VALUE)
+						.addComponent(txtSearch, GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE)
+						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 47, Short.MAX_VALUE)
+						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 47, Short.MAX_VALUE))
+					.addGap(17))
 		);
 		panel.setLayout(gl_panel);
 		
 		table = new JTable();
+		table.setShowGrid(false);
+		table.setShowHorizontalLines(false);
 		table.setFillsViewportHeight(true);
 		table.setShowVerticalLines(false);
 		table.setRowSelectionAllowed(false);
@@ -186,14 +191,14 @@ public class DefaultView extends JFrame implements View {
 		table.setCellSelectionEnabled(true);
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
+				{"A", "B", "C", "D", "E", "F", "G", "H"},
+				{"I", "J", "K", "L", "M", "N", "O", "P"},
+				{"Q", "R", "S", "T", "U", "V", "W", "X"},
+				{"Y", "Z", "0", "1", "2", "3", "4", "5"},
+				{"6", "7", "1", "9", "a", "b", "c", "d"},
+				{"e", "f", "g", "h", "i", "j", "k", "l"},
+				{"m", "n", "o", "p", "q", "r", "s", "t"},
+				{"u", "v", "w", "x", "y", "z", "@", "&"},
 			},
 			new String[] {
 				"New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column"
@@ -272,5 +277,20 @@ public class DefaultView extends JFrame implements View {
 			lastSearchFieldText = txtSearch.getText();
 			txtSearch.setText("  Search images...");
 		}
+	}
+	
+	private class MetroButton extends JLabel {
+		
+		public MetroButton(String text, Font font, Color unselected, Color selected, Color depressed) {
+			super(text);
+			setFocusable(true);
+			setFont(font);
+			setForeground(unselected);
+		}
+		
+		public void addActionListener(ActionListener listener) {
+			
+		}
+		
 	}
 }
