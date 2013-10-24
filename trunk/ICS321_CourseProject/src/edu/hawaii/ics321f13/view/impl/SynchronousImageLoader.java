1f989e1683db8ae56f68ac8e4b452cafb681beab
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-24 22:04:18 +0000

    Implemented synchronous image loading functionality.

--- trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/SynchronousImageLoader.java
+++ trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/SynchronousImageLoader.java
@@ -1,23 +1,45 @@
 package edu.hawaii.ics321f13.view.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import edu.hawaii.ics321f13.model.interfaces.ImageResult;
 import edu.hawaii.ics321f13.model.interfaces.Traversable;
+import edu.hawaii.ics321f13.model.interfaces.Traverser;
+import edu.hawaii.ics321f13.view.interfaces.ImageLoadListener;
 
 public class SynchronousImageLoader extends AbstractImageLoader {
 
 	@Override
 	public void close() throws IOException {
-		// TODO Auto-generated method stub
-
+		// There are no thread pools to close, so do nothing.
 	}
 
 	@Override
-	public int loadImages(Traversable<ImageResult> source, int loadCount)
-			throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int loadImages(Traversable<ImageResult> source, int loadCount) throws IOException {
+		ArrayList<ImageResult> loaded = new ArrayList<ImageResult>();
+		Traverser<ImageResult> images = source.traverser();
+		for(int i = 0; i < loadCount && images.hasNext(); i++) {
+			ImageResult result = images.next();
+			result.getImage(); // Load the actual image.
+			fireOnLoaded(result);
+		}
+		fireOnLoaded(loaded.toArray(new ImageResult[loaded.size()]));
+		return loaded.size();
+	}
+	
+	private void fireOnLoaded(ImageResult loaded) {
+		ImageLoadListener[] loadListeners = listeners.getListeners(ImageLoadListener.class);
+		for(ImageLoadListener loadListener : loadListeners) {
+			loadListener.onLoaded(loaded);
+		}
+	}
+	
+	private void fireOnLoaded(ImageResult[] loaded) {
+		ImageLoadListener[] loadListeners = listeners.getListeners(ImageLoadListener.class);
+		for(ImageLoadListener loadListener : loadListeners) {
+			loadListener.onLoaded(loaded);
+		}
 	}
 
 }
