fdd9b09c911e4dd9ed1b8b7c64ae2d65551178b2
808ethan <808ethan@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-25 05:59:07 +0000

    DefaultImageResult.getImage() implemented naively. jsoup.jar included for parsing html files.

--- trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/model/impl/DefaultImageResult.java
+++ trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/model/impl/DefaultImageResult.java
@@ -2,8 +2,15 @@ package edu.hawaii.ics321f13.model.impl;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Objects;
+
+import javax.imageio.ImageIO;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import edu.hawaii.ics321f13.model.interfaces.ImageResult;
 
@@ -39,7 +46,29 @@ public class DefaultImageResult implements ImageResult {
 	public BufferedImage getImage() {
 		// TODO Ethan, this is where you would put the code which loads the image from the webpage (or delegate it to a method in this class...whatever way you wanna do it).
 		// Just make sure you assign the imageCache variable to the BufferedImage that you get back from ImageIO and then return the imageCache.
-		return null;
+		String imageUrlString;
+		try {
+			URI page = new URI(getImageURL().toString());
+			Document doc = Jsoup.connect(page.toString()).get();
+			
+			Element image = doc.select("img").get(0); //0 will get the picture, but a little big
+			imageUrlString = image.absUrl("src");
+			/*  
+			//requires function passed image id name, not image url can get smaller images. Maybe nice for V2.0 of the project
+			imageUrlText = image.absUrl("src");
+			for(int i = 1; !imageUrlText.contains(imageName) && i < doc.select("img").size(); i++) { //check we didn't run out of links
+				image = doc.select("img").get(i);
+				imageUrlText = image.absUrl("src");
+				//System.out.println("Replacing image selection: " + imageUrlText);
+			}
+			*/
+			//System.out.println(imageUrlText);
+			imageCache = ImageIO.read(new URL(imageUrlString));
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return imageCache;
 	}
 
 	@Override
