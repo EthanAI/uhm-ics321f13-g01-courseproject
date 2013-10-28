93096524e1c2445ae8431090787a3d37eac06fb1
kytwogoo@gmail.com <kytwogoo@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-28 04:10:04 +0000

    Wrote sample javadoc to help Nate in writing documentation.

--- trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/model/interfaces/SearchableModel.java
+++ trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/model/interfaces/SearchableModel.java
@@ -3,7 +3,16 @@ package edu.hawaii.ics321f13.model.interfaces;
 import java.io.Closeable;
 
 public interface SearchableModel extends Closeable {
-	
+	/**
+	 * Queries the data model for values which match the specified search criteria. If no results are found, the
+	 * returned <code>Traversable</code> will be empty.
+	 * 
+	 * @param key - the search key.
+	 * @param resultType - the expected type of the returned results <code>Traversable</code>.
+	 * @param constraint - the <code>ResultConstraint</code> by which search results will be constrained.
+	 * 
+	 * @return A <code>Traversable</code> containing the results of this search.
+	 */
 	Traversable<ImageResult> search(String key, Class<?> resultType, ResultConstraint constraint);
 	
 }
