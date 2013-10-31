f60028f2022da13edaa0cba4dfec36f6eb6f5983
n.butler001@gmail.com <n.butler001@gmail.com@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-31 03:15:03 +0000

    LoginDialog update

--- ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/LoginDialog.java
+++ ICS321_CourseProject/src/edu/hawaii/ics321f13/view/impl/LoginDialog.java
@@ -2,12 +2,12 @@ package edu.hawaii.ics321f13.view.impl;
 
 import javax.swing.JDialog;
 
+import edu.hawaii.ics321f13.model.impl.DefaultLoginInfo;
 import edu.hawaii.ics321f13.model.interfaces.LoginInfo;
 import edu.hawaii.ics321f13.model.interfaces.LoginPrompt;
 
 import java.awt.Font;
 
-import javax.swing.JTextPane;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
 
@@ -16,32 +16,28 @@ import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.UIManager;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class LoginDialog extends JDialog implements LoginPrompt {
-	private String username;
 	private char[] password;
+	private String username;
 	public LoginDialog() {
 		setTitle("Login Dialog");
 		getContentPane().setLayout(null);
-		
-		txtpnUsername = new JTextPane();
-		txtpnUsername.setBorder(new CompoundBorder(new LineBorder(new Color(171, 173, 179)), UIManager.getBorder("CheckBoxMenuItem.border")));
-		txtpnUsername.setToolTipText("Enter username");
-		txtpnUsername.setForeground(SystemColor.activeCaptionText);
-		txtpnUsername.setFont(new Font("Segoe UI", Font.PLAIN, 25));
-		txtpnUsername.setBounds(131, 67, 215, 34);
-		getContentPane().add(txtpnUsername);
-		username = txtpnUsername.getText();
 		
 		JLabel lblUsernameTitle = new JLabel("Username:");
 		lblUsernameTitle.setForeground(SystemColor.activeCaption);
 		lblUsernameTitle.setFont(new Font("Segoe UI", Font.PLAIN, 25));
 		lblUsernameTitle.setBounds(10, 67, 126, 34);
 		getContentPane().add(lblUsernameTitle);
+		
+		usernameField = new JTextField();
+		usernameField.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
+		usernameField.setBounds(139, 67, 285, 33);
+		getContentPane().add(usernameField);
+		usernameField.setColumns(10);
+		username = usernameField.getText();
 		
 		JLabel lblPasswordTitle = new JLabel("Password:");
 		lblPasswordTitle.setForeground(SystemColor.activeCaption);
@@ -50,7 +46,7 @@ public class LoginDialog extends JDialog implements LoginPrompt {
 		getContentPane().add(lblPasswordTitle);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(131, 129, 215, 34);
+		passwordField.setBounds(131, 129, 215, 27);
 		getContentPane().add(passwordField);
 		password = passwordField.getPassword();
 		
@@ -63,6 +59,7 @@ public class LoginDialog extends JDialog implements LoginPrompt {
 		button.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 40));
 		button.setBounds(361, 119, 63, 44);
 		getContentPane().add(button);
+		
 		button.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -73,14 +70,13 @@ public class LoginDialog extends JDialog implements LoginPrompt {
 
 	/** Serialization support */
 	private static final long serialVersionUID = 1L;
-	//private JPasswordField passwordField;
-	private JTextPane txtpnUsername;
 	private JPasswordField passwordField;
+	private JTextField usernameField;
 	
 	@Override
 	//not quite sure if i did this right. I also don't know how i would test it sorry kyle
 	public LoginInfo getLoginInfo() {
-		LoginInfo log = (LoginInfo) txtpnUsername, passwordField;
+		DefaultLoginInfo log = new DefaultLoginInfo(username, password);
 		return log;
 	}
 }
