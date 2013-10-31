dd04f82137874228355664a2429ad1336052cb80
808ethan <808ethan@575abf92-eebe-ab4d-0907-bbde0f049a66>
2013-10-31 10:37:47 +0000

    Added the SQL code that is used by the executeQuery() function to get image names and after a series of calls, return a Traversable structure of images. Made a constant for the name of the table inside DefaultSearchableModel, probably needs to be moved elsewhere but should work for now. Attempted added some JavaDoc. Removed the DB2Database.java file

--- trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/model/impl/DefaultDataModelFactory.java
+++ trunk/ICS321_CourseProject/src/edu/hawaii/ics321f13/model/impl/DefaultDataModelFactory.java
@@ -7,8 +7,18 @@ import edu.hawaii.ics321f13.model.interfaces.Database;
 import edu.hawaii.ics321f13.model.interfaces.LoginInfo;
 import edu.hawaii.ics321f13.model.interfaces.SearchableModel;
 
+/**
+ * Factory that creates the <code>SearchableModel</code> that will be used by the controller
+ * Uses <code>DataModelFactory</code> interface
+ */
 public class DefaultDataModelFactory implements DataModelFactory {
 
+	/**
+	 * Creates the <code>SearchableModel</code> 
+	 * 
+	 * @param login - <code>LoginInfo</code> for the model
+	 * @param port - port for the model
+	 */
 	@Override
 	public SearchableModel fromLogin(LoginInfo login, int port) {
 		try {
